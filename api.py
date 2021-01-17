c3949f8169038ea95dc4b3c9466774c04e1931b6
Simon Boeger <simon@boegerweb.com>
2021-01-17 03:33:17 -0800

    changed Test to Data, added flasks-cors

--- api.py
+++ api.py
@@ -1,15 +1,17 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 import main
+import flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins="*")
 api = Api(app)
 
 parser = reqparse.RequestParser()
 parser.add_argument('latitude', type=str, required=True)
 parser.add_argument('longitude', type=str, required=True)
 
-class Test(Resource):
+class Data(Resource):
     def get(self):
         coordinates = {
                 'latitude': parser.parse_args().get('latitude','None'),
@@ -17,7 +19,7 @@ class Test(Resource):
                 }
         return main.getResults(coordinates)
 
-api.add_resource(Test,'/test/')
+api.add_resource(Data,'/data/')
 
 if __name__ == '__main__':
     app.run(debug=True)
