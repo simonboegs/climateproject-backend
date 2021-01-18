86fed5e44fb177331809c7eb2e1f1b81eb0682a3
Simon Boeger <simon@boegerweb.com>
2021-01-17 16:43:05 -0800

    fixed bug

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 import main
-import flask_cors import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, origins="*")
